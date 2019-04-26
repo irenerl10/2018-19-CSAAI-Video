7ea6ee79a104d4f3784e7bdd11d6c9c27643ab89
irenerl10 <irene17a@gmail.com>
2019-04-26 17:44:06 +0200

    segundo inntento putho github

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 2018-19-CSAAI-Video
+# 2018-19-CSAAI-
+https://irenerl10.github.io/2018-19-CSAAI-Video/
---
+++ video-01.js
@@ -0,0 +1,7 @@
+function main(){
+  window.onload = function (){ //cargando pestaefksjhfsjhñahhh//
+     var src = Math.floor(Math.random() * 4) + 1;
+     document.getElementById("video1").src = "videos/" + src + ".mp4";
+  }
+
+}
